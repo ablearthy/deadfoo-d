9930f38223936b0a09393f05d9dd1ba93ba57403
Able Arthy <ablearthy@gmail.com>
2023-05-21 18:38:07 +0300

    Add `ProductScan`

---
+++ lib/deadfood/scan/product_scan.cc
@@ -0,0 +1,53 @@
+#include "product_scan.hh"
+
+namespace deadfood::scan {
+
+ProductScan::ProductScan(std::unique_ptr<IScan> lhs, std::unique_ptr<IScan> rhs)
+    : lhs_{std::move(lhs)}, rhs_{std::move(rhs)} {}
+
+void ProductScan::BeforeFirst() {
+  lhs_->BeforeFirst();
+  lhs_->Next();
+  rhs_->BeforeFirst();
+}
+
+bool ProductScan::Next() {
+  if (rhs_->Next()) {
+    return true;
+  }
+  // no rows in rhs -> shift lhs
+  rhs_->BeforeFirst();
+  const auto l = lhs_->Next();
+  const auto r = rhs_->Next();
+  return l && r;
+}
+
+bool ProductScan::HasField(const std::string& field_name) const {
+  return lhs_->HasField(field_name) || rhs_->HasField(field_name);
+}
+
+core::FieldVariant ProductScan::GetField(const std::string& field_name) const {
+  if (lhs_->HasField(field_name)) {
+    return lhs_->GetField(field_name);
+  }
+  return rhs_->GetField(field_name);
+}
+
+void ProductScan::SetField(const std::string& field_name,
+                           const core::FieldVariant& value) {
+  if (lhs_->HasField(field_name)) {
+    lhs_->SetField(field_name, value);
+  } else {
+    rhs_->SetField(field_name, value);
+  }
+}
+
+void ProductScan::Insert() {}
+void ProductScan::Delete() {}
+
+void ProductScan::Close() {
+  lhs_->Close();
+  rhs_->Close();
+}
+
+}  // namespace deadfood::scan
---
+++ lib/deadfood/scan/product_scan.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <deadfood/scan/iscan.hh>
+
+namespace deadfood::scan {
+
+class ProductScan : public IScan {
+ public:
+  ProductScan(std::unique_ptr<IScan> lhs, std::unique_ptr<IScan> rhs);
+
+  void BeforeFirst() override;
+  bool Next() override;
+  bool HasField(const std::string& field_name) const override;
+  core::FieldVariant GetField(const std::string& field_name) const override;
+  void SetField(const std::string& field_name,
+                const core::FieldVariant& value) override;
+  void Insert() override;
+  void Delete() override;
+  void Close() override;
+ private:
+  std::unique_ptr<IScan> lhs_;
+  std::unique_ptr<IScan> rhs_;
+};
+
+}  // namespace deadfood::scan
