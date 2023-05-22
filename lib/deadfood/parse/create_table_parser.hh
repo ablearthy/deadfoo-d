2087d74566040ceb10c4a535ed68e7ec6c814ae9
Able Arthy <ablearthy@gmail.com>
2023-05-23 02:08:59 +0300

    Add `ParseCreateTableQuery`

---
+++ lib/deadfood/parse/create_table_parser.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <deadfood/core/constraint.hh>
+#include <deadfood/query/create_table_query.hh>
+#include <deadfood/lex/lex.hh>
+
+#include <stdexcept>
+
+namespace deadfood::parse {
+
+class CreateTableParseError : public std::runtime_error {
+ public:
+  using std::runtime_error::runtime_error;
+};
+
+std::pair<query::CreateTableQuery, std::vector<core::Constraint>>
+ParseCreateTableQuery(const std::vector<lex::Token>& tokens);
+
+}  // namespace deadfood::parse
