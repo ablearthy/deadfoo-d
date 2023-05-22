6d68995f0b5b55eb7b7ac13ea9facfffd0996e67
Able Arthy <ablearthy@gmail.com>
2023-05-22 23:41:42 +0300

    IExpr -> expr::IExpr

--- lib/deadfood/scan/extend_scan.hh
+++ lib/deadfood/scan/extend_scan.hh
@@ -7,7 +7,7 @@ namespace deadfood::scan {
 
 class ExtendScan : public IScan {
  public:
-  ExtendScan(std::unique_ptr<IScan> internal, std::unique_ptr<IExpr> expr, const std::string& name);
+  ExtendScan(std::unique_ptr<IScan> internal, std::unique_ptr<expr::IExpr> expr, const std::string& name);
   void BeforeFirst() override;
   bool Next() override;
   bool HasField(const std::string& field_name) const override;
