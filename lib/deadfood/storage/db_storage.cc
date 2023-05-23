7549cc4c756da8085f88d9b2cff8711d8b9d2aff
Able Arthy <ablearthy@gmail.com>
2023-05-23 10:26:19 +0300

    Add `DBStorage::Add` method

--- lib/deadfood/storage/db_storage.cc
+++ lib/deadfood/storage/db_storage.cc
@@ -1,10 +1,16 @@
 #include "db_storage.hh"
 
 namespace deadfood::storage {
+
 TableStorage& DBStorage::Get(const std::string& table_name) {
   return storage_[table_name];
 }
 bool DBStorage::Exists(const std::string& table_name) const {
   return storage_.contains(table_name);
 }
+
+void DBStorage::Add(const std::string& table_name) {
+  storage_.emplace(table_name, TableStorage{});
+}
+
 }  // namespace deadfood::storage
