228f50b2b0f581b7c3d84141449bbb57d0cad1a9
Able Arthy <ablearthy@gmail.com>
2023-05-23 10:38:27 +0300

    Start working on `Database` class

---
+++ lib/deadfood/database.cc
@@ -0,0 +1,58 @@
+#include "database.hh"
+
+#include <algorithm>
+
+namespace deadfood {
+
+void RemoveUnnecessaryConstraints(std::vector<core::Constraint>& constraints,
+                                  const std::string& slave_table_name) {
+  std::remove_if(
+      constraints.begin(), constraints.end(), [&](const core::Constraint& constr) {
+        if (!std::holds_alternative<core::ReferencesConstraint>(constr)) {
+          return false;
+        }
+        const auto& tbl =
+            std::get<core::ReferencesConstraint>(constr).slave_table;
+        return tbl == slave_table_name;
+      });
+}
+
+Database::Database(deadfood::storage::DBStorage& storage,
+                   std::map<std::string, core::Schema>& schemas,
+                   std::vector<core::Constraint>& constraints)
+    : storage_{std::move(storage)},
+      schemas_{std::move(schemas)},
+      constraints_{std::move(constraints)} {}
+
+std::vector<core::Constraint>& Database::constraints() { return constraints_; }
+
+bool Database::Exists(const std::string& table_name) const {
+  return schemas_.contains(table_name);
+}
+
+void Database::AddTable(const std::string& table_name,
+                        const core::Schema& schema) {
+  if (Exists(table_name)) {
+    return;
+  }
+  schemas_.emplace(table_name, schema);
+  storage_.Add(table_name);
+}
+
+void Database::RemoveTable(const std::string& table_name) {
+  if (!Exists(table_name)) {
+    return;
+  }
+  schemas_.erase(table_name);
+  storage_.Remove(table_name);
+  RemoveUnnecessaryConstraints(constraints_, table_name);
+}
+
+std::unique_ptr<scan::TableScan> Database::GetTableScan(
+    const std::string& table_name) {
+  auto& schema = schemas_.at(table_name);
+  auto& table_storage = storage_.Get(table_name);
+  return std::make_unique<scan::TableScan>(table_storage, schema);
+}
+
+}  // namespace deadfood
