4f96ecce7d513084a995c9fc9ee76a5af7947f1b
Able Arthy <ablearthy@gmail.com>
2023-05-23 10:46:09 +0300

    Add `Database::table_names`

--- lib/deadfood/database.hh
+++ lib/deadfood/database.hh
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <map>
+#include <filesystem>
 
 #include <deadfood/storage/db_storage.hh>
 #include <deadfood/core/schema.hh>
 #include <deadfood/core/constraint.hh>
 #include <deadfood/scan/table_scan.hh>
+#include <set>
 
 namespace deadfood {
 
@@ -17,7 +19,7 @@ class Database {
 
   std::vector<core::Constraint>& constraints();
 
-
+  const std::set<std::string>& table_names() const;
   [[nodiscard]] bool Exists(const std::string& table_name) const;
 
   void AddTable(const std::string& table_name, const core::Schema& schema);
@@ -27,8 +29,13 @@ class Database {
 
  private:
   storage::DBStorage storage_;
+  std::set<std::string> table_names_;
   std::map<std::string, core::Schema> schemas_;
   std::vector<core::Constraint> constraints_;
 };
+
+void Dump(const Database& db, const std::filesystem::path& path);
+
+Database Load(const std::filesystem::path& path);
 
 }  // namespace deadfood
