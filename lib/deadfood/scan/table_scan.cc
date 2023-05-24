f1e78e0eaa4ad78d49cd5d80c8b6736c09183227
Able Arthy <ablearthy@gmail.com>
2023-05-24 08:24:59 +0300

    Use `NormalizeFieldName` to avoid name collisions

--- lib/deadfood/scan/table_scan.cc
+++ lib/deadfood/scan/table_scan.cc
@@ -1,10 +1,14 @@
 #include "table_scan.hh"
-#include "deadfood/core/row.hh"
+
+#include <deadfood/core/row.hh>
+#include <algorithm>
 
 namespace deadfood::scan {
 
-TableScan::TableScan(storage::TableStorage& storage, const core::Schema& schema)
-    : storage_{storage},
+TableScan::TableScan(storage::TableStorage& storage, const core::Schema& schema,
+                     const std::string& table_name)
+    : table_name_{table_name},
+      storage_{storage},
       schema_{schema},
       it_{storage.storage().begin()},
       cur_row_should_be_deleted_{false},
@@ -44,19 +48,28 @@ bool TableScan::Next() {
   return true;
 }
 
+std::string TableScan::NormalizeFieldName(const std::string& field_name) const {
+  if (!field_name.starts_with(table_name_) ||
+      field_name.find('.') != table_name_.size() ||
+      field_name.size() <= table_name_.size() + 1) {
+    return field_name;
+  }
+  return field_name.substr(table_name_.size() + 1);
+}
+
 bool TableScan::HasField(const std::string& field_name) const {
-  return schema_.Exists(field_name);
+  return schema_.Exists(NormalizeFieldName(field_name));
 }
 
 core::FieldVariant TableScan::GetField(const std::string& field_name) const {
   const auto row = core::Row(it_->second, schema_);
-  return row.GetField(field_name);
+  return row.GetField(NormalizeFieldName(field_name));
 }
 
 void TableScan::SetField(const std::string& field_name,
                          const core::FieldVariant& value) {
   auto row = core::Row(it_->second, schema_);
-  row.SetField(field_name, value);
+  row.SetField(NormalizeFieldName(field_name), value);
 }
 
 void TableScan::Insert() {
