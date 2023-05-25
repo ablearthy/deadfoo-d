43f12b608dfd04e203d70d21f34e61f345022160
Able Arthy <ablearthy@gmail.com>
2023-05-25 23:29:15 +0300

    Change semantics of `BoolExpr`: now it may return `null`

--- lib/deadfood/expr/bin_bool_expr.cc
+++ lib/deadfood/expr/bin_bool_expr.cc
@@ -7,8 +7,36 @@ BinBoolExpr::BinBoolExpr(BinBoolOp op, std::unique_ptr<IExpr> lhs,
     : op_{op}, lhs_{BoolExpr(std::move(lhs))}, rhs_{BoolExpr(std::move(rhs))} {}
 
 core::FieldVariant BinBoolExpr::Eval() {
-  const auto left = std::get<bool>(lhs_.Eval());
-  const auto right = std::get<bool>(rhs_.Eval());
+  const auto left_eval = lhs_.Eval();
+  const auto right_eval = rhs_.Eval();
+
+  if (std::holds_alternative<core::null_t>(left_eval) &&
+      std::holds_alternative<core::null_t>(right_eval)) {
+    return core::null_t{};
+  }
+
+  if (std::holds_alternative<core::null_t>(left_eval)) {
+    switch (op_) {
+      case BinBoolOp::Xor:
+      case BinBoolOp::And:
+        return core::null_t{};
+      case BinBoolOp::Or:
+        return std::get<bool>(right_eval);
+    }
+  }
+
+  if (std::holds_alternative<core::null_t>(right_eval)) {
+    switch (op_) {
+      case BinBoolOp::Xor:
+      case BinBoolOp::And:
+        return core::null_t{};
+      case BinBoolOp::Or:
+        return std::get<bool>(left_eval);
+    }
+  }
+
+  const auto left = std::get<bool>(left_eval);
+  const auto right = std::get<bool>(right_eval);
   switch (op_) {
     case BinBoolOp::And:
       return left && right;
--- lib/deadfood/expr/bool_expr.cc
+++ lib/deadfood/expr/bool_expr.cc
@@ -8,16 +8,16 @@ BoolExpr::BoolExpr(std::unique_ptr<IExpr> internal)
 core::FieldVariant BoolExpr::Eval() {
   const auto val = internal_->Eval();
   return std::visit(
-      [](auto&& arg) {
+      [](auto&& arg) -> core::FieldVariant {
         using T = std::decay_t<decltype(arg)>;
 
         if constexpr (std::is_same_v<T, bool> || std::is_same_v<T, int> ||
                       std::is_same_v<T, float> || std::is_same_v<T, double>) {
           return static_cast<bool>(arg != 0);
-        } else if constexpr (std::is_same_v<T, std::span<char>>) {
+        } else if constexpr (std::is_same_v<T, std::string>) {
           return static_cast<bool>(arg.size() != 0);
-        } else {
-          return false;
+        } else if constexpr (std::is_same_v<T, core::null_t>) {
+          return core::null_t{};
         }
       },
       val);
