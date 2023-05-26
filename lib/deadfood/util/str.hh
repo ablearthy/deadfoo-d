dc13b0b61ab9ef15b2440f412d50de530f9d38e9
Able Arthy <ablearthy@gmail.com>
2023-05-26 09:58:18 +0300

    Add `util::lowercase`

--- lib/deadfood/util/str.hh
+++ lib/deadfood/util/str.hh
@@ -9,4 +9,13 @@ inline bool ContainsDot(const std::string& str) {
   return std::find(str.cbegin(), str.cend(), '.') != str.cend();
 }
 
+template <typename T>
+inline std::basic_string<T> lowercase(const std::basic_string<T>& s) {
+  std::basic_string<T> s2 = s;
+  std::transform(s2.begin(), s2.end(), s2.begin(), [](const T v) {
+    return static_cast<T>(std::tolower(static_cast<uint8_t>(v)));
+  });
+  return s2;
+}
+
 }  // namespace deadfood::util
