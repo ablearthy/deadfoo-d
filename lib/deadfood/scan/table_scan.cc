923ccdccb767d14bb27e2e8442247428193bcb9b
Able Arthy <ablearthy@gmail.com>
2023-05-28 09:43:09 +0300

    Simplify `TableScan`

--- lib/deadfood/scan/table_scan.cc
+++ lib/deadfood/scan/table_scan.cc
@@ -1,7 +1,7 @@
 #include "table_scan.hh"
 
 #include <deadfood/core/row.hh>
-#include <algorithm>
+#include <deadfood/util/parse.hh>
 
 namespace deadfood::scan {
 
@@ -11,80 +11,55 @@ TableScan::TableScan(storage::TableStorage& storage, const core::Schema& schema,
       storage_{storage},
       schema_{schema},
       it_{storage.storage().begin()},
-      cur_row_should_be_deleted_{false},
       before_start_{true} {}
 
 void TableScan::set_table_name(const std::string& table_name) {
   table_name_ = table_name;
 }
 
-void TableScan::BeforeFirst() {
-  if (cur_row_should_be_deleted_) {
-    storage_.storage().erase(it_);
-    cur_row_should_be_deleted_ = false;
-  }
-  before_start_ = true;
-}
+void TableScan::BeforeFirst() { before_start_ = true; }
 
 bool TableScan::Next() {
   if (before_start_) {
     before_start_ = false;
     it_ = storage_.storage().begin();
-    if (it_ == storage_.storage().end()) {
-      return false;
-    }
-    return true;
+    return it_ != storage_.storage().end();
   }
 
   if (it_ == storage_.storage().end()) {
     return false;
   }
 
-  auto new_it = it_;
-  ++new_it;
-  if (cur_row_should_be_deleted_) {
-    storage_.storage().erase(it_);
-    cur_row_should_be_deleted_ = false;
-  }
+  ++it_;
 
-  if (new_it == storage_.storage().end()) {
-    return false;
-  }
-
-  it_ = new_it;
-  return true;
-}
-
-std::string TableScan::NormalizeFieldName(const std::string& field_name) const {
-  if (!field_name.starts_with(table_name_) ||
-      field_name.find('.') != table_name_.size() ||
-      field_name.size() <= table_name_.size() + 1) {
-    return field_name;
-  }
-  return field_name.substr(table_name_.size() + 1);
+  return it_ != storage_.storage().end();
 }
 
 bool TableScan::HasField(const std::string& field_name) const {
-  return schema_.Exists(NormalizeFieldName(field_name));
+  const auto [tbl, field] = deadfood::parse::util::GetFullFieldName(field_name);
+  if (tbl.has_value() && tbl.value() != table_name_) {
+    return false;
+  }
+  return schema_.Exists(field);
 }
 
 core::FieldVariant TableScan::GetField(const std::string& field_name) const {
+  if (it_ == storage_.storage().end()) {
+    return core::null_t{};
+  }
   const auto row = core::Row(it_->second, schema_);
-  return row.GetField(NormalizeFieldName(field_name));
+  const auto [_, field] = deadfood::parse::util::GetFullFieldName(field_name);
+  return row.GetField(field);
 }
 
 void TableScan::SetField(const std::string& field_name,
                          const core::FieldVariant& value) {
   auto row = core::Row(it_->second, schema_);
-  row.SetField(NormalizeFieldName(field_name), value);
+  const auto [_, field] = deadfood::parse::util::GetFullFieldName(field_name);
+  row.SetField(field, value);
 }
 
 void TableScan::Insert() {
-  if (cur_row_should_be_deleted_) {
-    storage_.storage().erase(it_);
-    cur_row_should_be_deleted_ = false;
-  }
-
   size_t row_id = 0;
   if (!storage_.storage().empty()) {
     auto last = storage_.storage().end();
@@ -97,7 +72,22 @@ void TableScan::Insert() {
   it_ = new_it;
 }
 
-void TableScan::Delete() { cur_row_should_be_deleted_ = true; }
+void TableScan::Delete() {
+  if (before_start_ || storage_.storage().empty() ||
+      it_ == storage_.storage().end()) {
+    return;
+  }
+
+  auto copy_it = it_;
+  if (storage_.storage().begin() == it_) {
+    ++copy_it;
+  } else {
+    --copy_it;
+  }
+
+  storage_.storage().erase(it_);
+  it_ = copy_it;
+}
 
 void TableScan::Close() {}
 
