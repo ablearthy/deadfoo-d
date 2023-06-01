59befd6f37d945f0eb6f60fe2cec72088a0e09d5
Able Arthy <ablearthy@gmail.com>
2023-06-01 23:04:00 +0300

    Rewrite `INSERT INTO` parser using utils

--- lib/deadfood/parse/insert_parser.cc
+++ lib/deadfood/parse/insert_parser.cc
@@ -6,74 +6,65 @@
 
 #include <deadfood/util/parse.hh>
 #include <deadfood/util/str.hh>
+#include <deadfood/parse/parse_util.hh>
 
 namespace deadfood::parse {
 
-query::InsertQuery ParseInsertQuery(const std::vector<lex::Token>& tokens) {
-  if (tokens.size() <= 4 || !lex::IsKeyword(tokens[0], lex::Keyword::Insert) ||
-      !lex::IsKeyword(tokens[1], lex::Keyword::Into)) {
-    throw ParserError("invalid query");
-  }
-  if (!std::holds_alternative<lex::Identifier>(tokens[2])) {
-    throw ParserError("expected table table name");
-  }
-  auto table_name = std::get<lex::Identifier>(tokens[2]).id;
-  util::RaiseParserErrorIf(deadfood::util::ContainsDot(table_name),
-                           "invalid name of table");
-  auto it = tokens.begin() + 3;
+template <std::forward_iterator It>
+  requires std::is_same_v<std::iter_value_t<It>, lex::Token>
+query::InsertQuery ParseInsertQueryInternal(It& it, const It end) {
+  util::ParseKeyword(it, end, lex::Keyword::Insert);
+  util::ParseKeyword(it, end, lex::Keyword::Into);
+  const auto table_name = util::ParseIdWithoutDot(it, end);
+
   std::optional<std::vector<std::string>> field_names = std::nullopt;
   if (lex::IsSymbol(*it, lex::Symbol::LParen)) {
-    field_names = std::vector<std::string>{};
     ++it;
-    while (!lex::IsSymbol(*it, lex::Symbol::RParen)) {
-      util::RaiseParserErrorIf(!std::holds_alternative<lex::Identifier>(*it),
-                               "expected field name");
-
-      auto field_name = std::get<lex::Identifier>(*it).id;
+    auto field_name = util::ParseIdWithoutDot(it, end);
+    field_names = {field_name};
+    while (lex::IsSymbol(*it, lex::Symbol::Comma)) {
+      ++it;  // comma
+      field_name = util::ParseIdWithoutDot(it, end);
       field_names->emplace_back(field_name);
-      util::RaiseParserErrorIf(deadfood::util::ContainsDot(field_name),
-                               "invalid name of field");
-      ++it;
-      util::ExpectNotEnd(it, tokens.end(), "expected `)`");
-      if (lex::IsSymbol(*it, lex::Symbol::Comma)) {
-        ++it;
-      }
-      util::ExpectNotEnd(it, tokens.end(), "expected `)`");
     }
-    ++it;
+    util::ParseSymbol(it, end, lex::Symbol::RParen);
   }
 
-  if (it == tokens.end() || !lex::IsKeyword(*it, lex::Keyword::Values)) {
-    throw ParserError("expected `VALUES`");
-  }
-  ++it;
+  util::ParseKeyword(it, end, lex::Keyword::Values);
   std::vector<std::vector<expr::FactorTree>> values;
-  while (it != tokens.end()) {
-    util::ExpectSymbol(it, tokens.end(), lex::Symbol::LParen, "expected `(`");
-    ++it;
-    util::ExpectNotEnd(it, tokens.end());
+  while (true) {
     std::vector<expr::FactorTree> row;
-    while (!lex::IsSymbol(*it, lex::Symbol::RParen)) {
-      auto expr = ParseExprTree(it, tokens.end());
+    util::ParseSymbol(it, end, lex::Symbol::LParen);
+
+    while (true) {
+      auto expr = ParseExprTree(it, end);
       row.emplace_back(std::move(expr));
-      util::ExpectNotEnd(it, tokens.end());
+
       if (lex::IsSymbol(*it, lex::Symbol::Comma)) {
-        ++it;
-      } else if (!lex::IsSymbol(*it, lex::Symbol::RParen)) {
-        throw ParserError("invalid expression");
+        util::ParseSymbol(it, end, lex::Symbol::Comma);
+      } else {
+        break;
       }
     }
     values.emplace_back(std::move(row));
-    ++it;
-    if (it == tokens.end()) {
-      break;
+    util::ParseSymbol(it, end, lex::Symbol::RParen);
+
+    if (lex::IsSymbol(*it, lex::Symbol::Comma)) {
+      util::ParseSymbol(it, end, lex::Symbol::Comma);
     } else {
-      util::ExpectSymbol(it, tokens.end(), lex::Symbol::Comma, "expected `,`");
-      ++it;
+      break;
     }
   }
+
   return query::InsertQuery{
       .table_name = table_name, .fields = field_names, .values = values};
+}
+
+query::InsertQuery ParseInsertQuery(const std::vector<lex::Token>& tokens) {
+  auto it = tokens.begin();
+  const auto ret = ParseInsertQueryInternal(it, tokens.end());
+  util::RaiseParserErrorIf(it != tokens.end(), "unexpected end");
+  return ret;
 }
 
 }  // namespace deadfood::parse
